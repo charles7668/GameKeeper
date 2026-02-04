c20531dac725159fd783a1a54166e18f564172cc
charles7668 <charles76680119@gmail.com>
2026-02-04 15:26:41 +0800

    temp

--- GameKeeper/ViewModels/MainViewModel.cs
+++ GameKeeper/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -12,17 +13,41 @@ public partial class MainViewModel : ObservableObject
         Refresh();
     }
 
+    private const int GWLP_WNDPROC = -4;
+
     [ObservableProperty]
     private ObservableCollection<Process> _processes = [];
 
     [ObservableProperty]
     private Process? _selectedProcess;
 
+    [ObservableProperty]
+    private nint _wndProcAddress;
+
     [RelayCommand]
     private void Attach()
     {
-        // TODO: Implement Attach logic
+        if (SelectedProcess != null)
+        {
+            WndProcAddress = GetWindowLongPtr(SelectedProcess.MainWindowHandle, GWLP_WNDPROC);
+        }
     }
+
+    [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+    private static extern nint GetWindowLong32(nint hWnd, int nIndex);
+
+    private static nint GetWindowLongPtr(nint hWnd, int nIndex)
+    {
+        if (IntPtr.Size == 8)
+        {
+            return GetWindowLongPtr64(hWnd, nIndex);
+        }
+
+        return GetWindowLong32(hWnd, nIndex);
+    }
+
+    [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr")]
+    private static extern nint GetWindowLongPtr64(nint hWnd, int nIndex);
 
     [RelayCommand]
     private void Refresh()
